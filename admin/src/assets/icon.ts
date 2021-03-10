2820182783a9c5962c5b0066630a4576f5a33a26
chieveit <javachieveit@163.com>
2021-03-10 14:52:21 +0000

    创建admin和mobile模板

---
+++ admin/src/assets/icon.ts
@@ -0,0 +1,7 @@
+import { createFromIconfontCN } from '@ant-design/icons';
+
+const Icon = createFromIconfontCN({
+  scriptUrl: 'https://at.alicdn.com/t/font_2381931_tzvp56bh5il.js',
+});
+
+export default Icon;
