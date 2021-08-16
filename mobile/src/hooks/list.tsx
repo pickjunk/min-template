ba859f85fa0902bf23974023a899e242009127c7
chieveit <javachieveit@163.com>
2021-08-16 03:44:30 +0000

    update

--- mobile/src/hooks/list.tsx
+++ mobile/src/hooks/list.tsx
@@ -143,5 +143,8 @@ export default function useList({
     load: refreshList,
     refreshList,
     refreshItem: loadItem ? refreshItem : undefined,
+    item(i: number) {
+      return s.data[i];
+    },
   };
 }
